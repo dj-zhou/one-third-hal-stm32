c9d92796f4c309867b29c2ba6c702676d7846b79
Dingjiang Zhou <dingjiang.zhou@berkshiregrey.com>
2021-08-13 22:02:16 -0400

    wip: run "./make.sh hal" to make hal library

--- examples/make.sh
+++ examples/make.sh
@@ -6,12 +6,13 @@ for i in $directories ; do
     if [[ -d $i ]] && [[ -f $i/Makefile ]] ; then
         index=$((index+1))
 		echo -e "\n---------------------------------------"
-		echo -e " directory #$index:\033[0;32m $i\033[0m\n"
+		echo -e "(below) directory #$index:\033[0;32m $i\033[0m\n"
         if [ -z $1 ] ; then
 		    sleep 1
         fi
         cd $i
         make -j$(cat /proc/cpuinfo | grep processor | wc -l) $1
+		echo -e "(above) directory #$index:\033[0;32m $i\033[0m\n"
         cd ..
     fi
 done
