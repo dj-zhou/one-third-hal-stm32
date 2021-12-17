cfa0c88ebafd6b340692e6b088701ce9e55fac9b
Dingjiang Zhou <zhoudingjiang@gmail.com>
2021-12-16 19:38:46 -0500

    the attached callback function for CAN IRQ does not need to check the ID again

--- examples/012-can/f407zg/src/main.c
+++ examples/012-can/f407zg/src/main.c
@@ -51,9 +51,6 @@ static void taskCan2Test(void) {
 
 // ============================================================================
 static void Can1IrqTest(CAN_RxHeaderTypeDef* msg, uint8_t* data) {
-    if (msg->StdId != 0x666) {
-        return;
-    }
     console.printf(GRN "%s: " NOC, __func__);
     for (int i = 0; i < msg->DLC; i++) {
         console.printf(" 0x%02X", data[i]);
@@ -62,9 +59,7 @@ static void Can1IrqTest(CAN_RxHeaderTypeDef* msg, uint8_t* data) {
 }
 // ============================================================================
 static void Can2IrqTest(CAN_RxHeaderTypeDef* msg, uint8_t* data) {
-    if (msg->StdId != 0x0AA) {
-        return;
-    }
+
     console.printf(GRN "%s: " NOC, __func__);
     for (int i = 0; i < msg->DLC; i++) {
         console.printf(" 0x%02X", data[i]);
@@ -88,7 +83,7 @@ int main(void) {
     can2.config(1000, CAN_MODE_NORMAL);
 
     can1.irq.attach(0x666, Can1IrqTest, "Can1IrqTest");
-    can2.irq.attach(0x0AA, Can2IrqTest, "Can2IrqTest");
+    can2.irq.attach(0x0AB, Can2IrqTest, "Can2IrqTest");
 
     // tasks -----------
     stime.scheduler.attach(2000, 1, taskCan1Test, "taskCan1Test");
