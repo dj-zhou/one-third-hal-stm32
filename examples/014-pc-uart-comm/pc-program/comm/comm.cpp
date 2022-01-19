dfe7a47a8c9dcf338acccd05e37444fa9822f744
Dingjiang Zhou <zhoudingjiang@gmail.com>
2022-01-18 21:43:58 -0500

    apply format

--- examples/014-pc-uart-comm/pc-program/comm/comm.cpp
+++ examples/014-pc-uart-comm/pc-program/comm/comm.cpp
@@ -11,19 +11,19 @@ PcComm::PcComm(Serial* serial) {
 }
 
 void PcComm::send(const char* str, size_t len) {
-    serial_->send(( char* )str, len);
+    serial_->send((char*)str, len);
 }
 
 void PcComm::send(const CommVelCmd_t cmd) {
     size_t size = sizeof(cmd);
     size_t packet_size = size + 9;
-    comm_serialize(( const char* )&cmd, size, packet_);
-    serial_->send(( const char* )packet_, packet_size, 1000);
+    comm_serialize((const char*)&cmd, size, packet_);
+    serial_->send((const char*)packet_, packet_size, 1000);
 }
 
 void PcComm::send(const CommSwitchMode_t mode) {
     size_t size = sizeof(mode);
     size_t packet_size = size + 9;
-    comm_serialize(( const char* )&mode, size, packet_);
-    serial_->send(( const char* )packet_, packet_size, 1000);
+    comm_serialize((const char*)&mode, size, packet_);
+    serial_->send((const char*)packet_, packet_size, 1000);
 }
