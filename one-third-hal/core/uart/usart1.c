29e0cd479ed213582926fd58d5ebc1c5ef34af0f
Dingjiang Zhou <zhoudingjiang@gmail.com>
2022-01-18 20:28:08 -0500

    apply format to files

--- one-third-hal/core/uart/usart1.c
+++ one-third-hal/core/uart/usart1.c
@@ -68,8 +68,8 @@ static void Usart1RingConfig(uint8_t* data, uint16_t len) {
 
 // ----------------------------------------------------------------------------
 static void Usart1DmaConfig(uint8_t* buffer, uint16_t len) {
-    ( void )buffer;
-    ( void )len;
+    (void)buffer;
+    (void)len;
     // if we use DMA for receiving, we don't want UART_IT_RXNE
     __HAL_UART_DISABLE_IT(&(usart1.huart), UART_IT_RXNE);
     __HAL_RCC_DMA2_CLK_ENABLE();
@@ -94,7 +94,7 @@ static void Usart1DmaConfig(uint8_t* buffer, uint16_t len) {
     // only use verified code
 #if defined(STM32F407xx)
     HAL_DMA_Start(&hdma_usart1_rx, (uint32_t) & (usart1.huart.Instance->DR),
-                  ( uint32_t )buffer, len);
+                  (uint32_t)buffer, len);
 #elif defined(STM32F767xx)
     // HAL_DMA_Start(&hdma_usart2_rx, (uint32_t) & (usart2.huart.Instance->RDR),
     //               ( uint32_t )buffer, len);
--- one-third-hal/core/uart/usart2.c
+++ one-third-hal/core/uart/usart2.c
@@ -76,8 +76,8 @@ static void Usart2RingConfig(uint8_t* data, uint16_t len) {
 }
 // ----------------------------------------------------------------------------
 static void Usart2DmaConfig(uint8_t* buffer, uint16_t len) {
-    ( void )buffer;
-    ( void )len;
+    (void)buffer;
+    (void)len;
     // if we use DMA for receiving, we don't want UART_IT_RXNE
     __HAL_UART_DISABLE_IT(&(usart2.huart), UART_IT_RXNE);
     __HAL_RCC_DMA1_CLK_ENABLE();
@@ -102,10 +102,10 @@ static void Usart2DmaConfig(uint8_t* buffer, uint16_t len) {
     // only use verified code
 #if defined(STM32F407xx)
     HAL_DMA_Start(&hdma_usart2_rx, (uint32_t) & (usart2.huart.Instance->DR),
-                  ( uint32_t )buffer, len);
+                  (uint32_t)buffer, len);
 #elif defined(STM32F767xx)
     HAL_DMA_Start(&hdma_usart2_rx, (uint32_t) & (usart2.huart.Instance->RDR),
-                  ( uint32_t )buffer, len);
+                  (uint32_t)buffer, len);
 #endif
 
     HAL_UART_Receive_DMA(&(usart2.huart), buffer, len);
