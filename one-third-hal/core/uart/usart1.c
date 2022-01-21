dj-zhou/one-third-hal-stm32

commit 84a4906967e700c2ac5e00ad62a29f1f6198646a

f767zi dma on usart2 rx works now, bug fixed!

one-third-hal/core/uart/usart1.c
@@ -91,16 +91,19 @@ static void Usart1DmaConfig(uint8_t* buffer, uint16_t len) {
     HAL_NVIC_SetPriority(DMA2_Stream2_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(DMA2_Stream2_IRQn);
 
+    // for F767ZI DMA-USART-RX to work, this line must be on the top then
+    // HAL_DMA_Start(), for F407ZG, it does not matter
+    HAL_UART_Receive_DMA(&(usart1.huart), buffer, len);
     // only use verified code
 #if defined(STM32F407xx)
     HAL_DMA_Start(&hdma_usart1_rx, (uint32_t) & (usart1.huart.Instance->DR),
                   (uint32_t)buffer, len);
 #elif defined(STM32F767xx)
+#error "not verified"
     // HAL_DMA_Start(&hdma_usart1_rx, (uint32_t) & (usart1.huart.Instance->RDR),
     //               ( uint32_t )buffer, len);
 #endif
 
-    HAL_UART_Receive_DMA(&(usart1.huart), buffer, len);
     usart1_dma_is_used = true;
     Usart1RingConfig(buffer, len);
 }

one-third-hal/core/uart/usart2.c
@@ -99,6 +99,9 @@ static void Usart2DmaConfig(uint8_t* buffer, uint16_t len) {
     HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
 
+    // for F767ZI DMA-USART-RX to work, this line must be on the top then
+    // HAL_DMA_Start(), for F407ZG, it does not matter
+    HAL_UART_Receive_DMA(&(usart2.huart), buffer, len);
     // only use verified code
 #if defined(STM32F407xx)
     HAL_DMA_Start(&hdma_usart2_rx, (uint32_t) & (usart2.huart.Instance->DR),
@@ -108,7 +111,6 @@ static void Usart2DmaConfig(uint8_t* buffer, uint16_t len) {
                   (uint32_t)buffer, len);
 #endif
 
-    HAL_UART_Receive_DMA(&(usart2.huart), buffer, len);
     usart2_dma_is_used = true;
     Usart2RingConfig(buffer, len);
 }

commit 877aba25b34939fdeb21bd7bb8acc5947b6ab5b5

fix a bug introduced in commit 84a4906967e700c2a: for F407XX, HAL_UART_Receive_DMA() cannot be on in front of HAL_DMA_Start(), however, for F767xx, it is reversed

one-third-hal/core/uart/usart1.c
@@ -91,13 +91,12 @@ static void Usart1DmaConfig(uint8_t* buffer, uint16_t len) {
     HAL_NVIC_SetPriority(DMA2_Stream2_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(DMA2_Stream2_IRQn);
 
-    // for F767ZI DMA-USART-RX to work, this line must be on the top then
-    // HAL_DMA_Start(), for F407ZG, it does not matter
-    HAL_UART_Receive_DMA(&(usart1.huart), buffer, len);
-    // only use verified code
 #if defined(STM32F407xx)
+    // for F407xx, HAL_UART_Receive_DMA() CAN NOT be in front
+    // usart1 is tested
     HAL_DMA_Start(&hdma_usart1_rx, (uint32_t) & (usart1.huart.Instance->DR),
                   (uint32_t)buffer, len);
+    HAL_UART_Receive_DMA(&(usart1.huart), buffer, len);
 #elif defined(STM32F767xx)
 #error "not verified"
     // HAL_DMA_Start(&hdma_usart1_rx, (uint32_t) & (usart1.huart.Instance->RDR),

one-third-hal/core/uart/usart2.c
@@ -99,14 +99,16 @@ static void Usart2DmaConfig(uint8_t* buffer, uint16_t len) {
     HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
 
-    // for F767ZI DMA-USART-RX to work, this line must be on the top then
-    // HAL_DMA_Start(), for F407ZG, it does not matter
-    HAL_UART_Receive_DMA(&(usart2.huart), buffer, len);
-    // only use verified code
 #if defined(STM32F407xx)
+    // for F407xx, HAL_UART_Receive_DMA() CAN NOT be in front
+    // usart2 is tested
     HAL_DMA_Start(&hdma_usart2_rx, (uint32_t) & (usart2.huart.Instance->DR),
                   (uint32_t)buffer, len);
+    HAL_UART_Receive_DMA(&(usart2.huart), buffer, len);
 #elif defined(STM32F767xx)
+    // for F767xx, HAL_UART_Receive_DMA() has to be in front
+    // usart2 is tested
+    HAL_UART_Receive_DMA(&(usart2.huart), buffer, len);
     HAL_DMA_Start(&hdma_usart2_rx, (uint32_t) & (usart2.huart.Instance->RDR),
                   (uint32_t)buffer, len);
 #endif